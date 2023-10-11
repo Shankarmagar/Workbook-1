58880e8396065228c4fac4b176dbfa26781f1b48
Shankar Ale Magar <shankaralemagar91@gmail.com>
2023-10-11 12:07:28 -0400

    third Excercise

---
+++ workbook-1/BasicCalculator/src/main/java/org/example/Main.java
@@ -0,0 +1,58 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the first number: ");
+        int firstNumber = scanner.nextInt();
+        System.out.println("Enter the second number");
+        int secondNumber = scanner.nextInt();
+     System.out.println("Possible calculations: ");
+     System.out.println("(A)dd");
+     System.out.println("(S)ubtract");
+     System.out.println("(M)ultiply");
+     System.out.println("(D)ivide");
+     System.out.println("Enter the value based on given prompt for calculation:");
+
+     char UserInput = scanner.next().charAt(0);
+     int answer;
+
+     if(UserInput == 'A')
+     {
+         //Do addition
+          answer = firstNumber + secondNumber;
+     }
+     else if(UserInput == 'S')
+     {
+         //Subtract
+          answer = firstNumber - secondNumber;
+
+     }
+     else if (UserInput == 'M')
+     {
+         //Do multiplication
+          answer = firstNumber * secondNumber;
+     }
+     else
+     {
+         //Do Divide
+          answer = secondNumber/firstNumber;
+     }
+
+     System.out.println("After calculation, your answer is : "+ answer);
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
